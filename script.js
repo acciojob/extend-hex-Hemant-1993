979ee7563d74956815448e9fc3e1171a860777b3
hemant modi <142676792+Hemant-1993@users.noreply.github.com>
2023-12-08 10:45:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const extendHex = (shortHex) => {
-  // write your code here
+  if(shortHex.length ===4){
+	  let ans =`#${shortHex[1]}${shortHex[1]}${shortHex[2]}${shortHex[2]}${shortHex[3]}${shortHex[3]}`
+	  return ans;
+  }
+	else{
+		 let ans =`#${shortHex[0]}${shortHex[0]}${shortHex[1]}${shortHex[1]}${shortHex[2]}${shortHex[2]}`
+	  return ans;
+	}
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+//const shortHex = prompt("Enter Short Hex.");
+//alert(extendHex(shortHex));
